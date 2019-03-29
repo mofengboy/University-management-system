3cea5df9d99adafad7b93dd5366a829d1272fff4
陌风小同学 <me@sunan.me>
2019-03-29 19:58:58 +0800

    评分页面完成

--- pages/check/check.js
+++ pages/check/check.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    photoUrl:'/images/default.png' 
+        realName:'',
+        photoUrl:'/images/default.png' 
   },
 
   /**
@@ -19,8 +20,24 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
-  },
+    wx.getStorage({
+      key: 'token',
+      success:res=> {
+        wx.request({
+          url: 'https://college.netlab.sunan.me/user/info/basic',
+          method:'POST',
+          data:{
+            token:res.data
+          },
+          success:res=>{
+            this.setData({
+              realName:res.data.real_name
+            })
+          }
+        })
+      },
+    })
+      },
 
   /**
    * 生命周期函数--监听页面显示
@@ -82,31 +99,55 @@ Page({
       },
     })
   },
+  // 确认
   confirm: function (e) {
-    console.log(e)
-    let _this = this;
-    wx.getStorage({
-      key: 'token',
-      success: res => {
-        console.log(this.data)
-        wx.request({
-          url: 'http://college.netlab.sunan.me/wechat/index/check',
-          method: 'POST',
-          data: {
-            token: res.data,
-            building: e.detail.value.building,
-            room: e.detail.value.room,
-            scores: e.detail.value.scores,
-
+    let nowDate = new Date();
+    let checkDateYear = nowDate.getFullYear();
+    let checkDate = checkDateYear.toString()+ (nowDate.getMonth()+1)+nowDate.getDate();
+    let checkTime = nowDate.toLocaleTimeString();
+    wx.request({
+      url: 'https://college.netlab.sunan.me/wechat/index/check',
+      method:'POST',
+      data:{
+        checkTime: checkTime,
+        checkDate: checkDate,
+        realName: this.data.realName,
+        building: e.detail.value.building,
+        room: e.detail.value.room,
+        scores: e.detail.value.scores
+      },
+      success:res=>{
+        //信息存储成功之后，开始上传文件
+        if(res.data.status == 1){
+          let id = res.data.id;
+        const uploadTask = wx.uploadFile({
+          url: 'https://college.netlab.sunan.me/wechat/index/check_img',
+          header: {
+            "content-type": 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+          filePath: this.data.photoUrl,
+          name: 'photoName',
+          formData:{
+            id:id
           },
           success: res => {
-            console.log(res)
-            this.setData({
-
-            })
+            if(res.data == 0){
+              wx.redirectTo({
+                url: '/pages/main/main',
+              })
+            }
           }
         })
+        uploadTask.onProgressUpdate((res) => {
+          console.log('上传进度', res.progress)
+          console.log('已经上传的数据长度', res.totalBytesSent)
+          console.log('预期需要上传的数据总长度', res.totalBytesExpectedToSend)
+        })
+      }else{
+        console.log("信息存储失败")
+      }
       }
     })
-  },
+   
+  }
 })
