c2ec69a4b87333482250ac1daa0654d37fbd8e27
fantomg <386015805@qq.com>
2019-04-15 22:55:14 +0800

    日期选择器修改稿

--- pages/history/history.js
+++ pages/history/history.js
@@ -109,5 +109,12 @@ Page({
    */
   onShareAppMessage: function () {
 
-  } 
+  },
+  bindDateChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+ 
 })
